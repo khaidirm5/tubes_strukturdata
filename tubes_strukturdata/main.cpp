9ff697c38bee9cc2634e8419998344cb25628052
khaidirm5 <vtq55g6xmw@privaterelay.appleid.com>
2025-12-06 23:42:55 +0700

    initial commit

---
+++ tubes_strukturdata/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  tubes_strukturdata
+//
+//  Created by KHAIDIR MAULANA on 06/12/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
